caf512e0b794af487765ba93cabc98c7f4f7f40f
Lina ElTohamy <Lina.ElTohamy@itworx.com>
2019-04-01 19:54:40 +0200

    Updates

---
+++ precache-manifest.a14b783002905dcd3934bf2794568f7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a21333eda49a28caf319e9dfda698c1d",
+    "url": "/Zeww-Workspace/static/media/logoplaceholder.a21333ed.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Zeww-Workspace/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "ea334ef53f5ee2931779",
+    "url": "/Zeww-Workspace/static/js/runtime~main.c0383199.js"
+  },
+  {
+    "revision": "07858a1f9626fc3c589b",
+    "url": "/Zeww-Workspace/static/js/main.e0798ceb.chunk.js"
+  },
+  {
+    "revision": "6ce2166fecad8e63e206",
+    "url": "/Zeww-Workspace/static/js/2.88286630.chunk.js"
+  },
+  {
+    "revision": "07858a1f9626fc3c589b",
+    "url": "/Zeww-Workspace/static/css/main.66541487.chunk.css"
+  },
+  {
+    "revision": "6ce2166fecad8e63e206",
+    "url": "/Zeww-Workspace/static/css/2.2fccc51a.chunk.css"
+  },
+  {
+    "revision": "0f789f97737f59e5619a5716ff883341",
+    "url": "/Zeww-Workspace/index.html"
+  }
+];
